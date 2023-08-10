d50941f7dafcb8e3d0f90c46e8c482c63d08f33f
1mmanuelKant <joselisboaa05@gmail.com>
2023-08-10 12:34:18 -0300

    fix: server response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-   return response.json({msg: "server working!"});
+   return res.json({msg: "server working!"});
 });
 
 app.listen(3333)
